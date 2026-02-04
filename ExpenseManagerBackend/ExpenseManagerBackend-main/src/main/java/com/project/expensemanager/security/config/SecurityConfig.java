7f8325499602eae233f3efbf1d06777a7f102664
Makarand Goralkar <makarandgoralkar27@gmail.com>
2026-02-04 21:35:51 +0530

    Fix CORS for localhost and Netlify

--- ExpenseManagerBackend/ExpenseManagerBackend-main/src/main/java/com/project/expensemanager/security/config/SecurityConfig.java
+++ ExpenseManagerBackend/ExpenseManagerBackend-main/src/main/java/com/project/expensemanager/security/config/SecurityConfig.java
@@ -87,7 +87,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of(frontendUrl));
+        config.setAllowedOrigins(List.of("http://localhost:3000",
+                "https://expensesmanagersystem.netlify.app"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setExposedHeaders(List.of("Authorization"));
